grp-bork/nIMP3

commit 7deadf44906db6afd1ff4049f3f0ea34ac4f49a4

fix post assembly routing blockage

imp/workflows/meta_t.nf
@@ -56,9 +56,10 @@ workflow metaT_initial_assembly {
 				def new_sample = sample.clone()
 				new_sample.id = sample.index_id
 				new_sample.remove("index_id")
-				return [ new_sample, [fastqs].flatten() ]
+				return [ new_sample.id, new_sample, [fastqs].flatten() ]
 			}
 			.groupTuple(by: 0, size: 2, remainder: true, sort: true)
+			.map { sample_id, sample, fastqs -> [ sample, fastqs ] }
 		unmapped_ch.dump(pretty: true, tag: "unmapped_after_metaT_assembly_1")
 
 		unmapped_ch = unmapped_ch

commit ab0a25b9d59669f3ebfe628161955de7066d853c

fix routing issue introduced in 7deadf4

imp/workflows/meta_t.nf
@@ -58,7 +58,7 @@ workflow metaT_initial_assembly {
 				new_sample.remove("index_id")
 				return [ new_sample.id, new_sample, [fastqs].flatten() ]
 			}
-			.groupTuple(by: 0, size: 2, remainder: true, sort: true)
+			.groupTuple(by: 0, size: 2, remainder: true) //, sort: true)
 			.map { sample_id, sample, fastqs -> [ sample, fastqs ] }
 		unmapped_ch.dump(pretty: true, tag: "unmapped_after_metaT_assembly_1")
 